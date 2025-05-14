b68bd198a4c20d2519a2f61550aee7ff729b23b1
bivas_ball-at-442815485291 <bball@uxli.com>
2025-05-14 22:50:37 +0530

    worked

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_HOSTNAME = os.getenv("DATABASE_HOSTNAME")
-DATABASE_PORT = os.getenv("DATABASE_PORT")
+DATABASE_PORT = int(os.getenv("DATABASE_PORT", 5432))  # Default to 5432 if not set
 DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
 DATABASE_NAME = os.getenv("DATABASE_NAME")
 DATABASE_USERNAME = os.getenv("DATABASE_USERNAME")
