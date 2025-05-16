c5339ec414a62857ad7b50fd6db1360cdc8e54ed
bivas_ball-at-442815485291 <bball@uxli.com>
2025-05-16 16:38:11 +0530

    save

--- app/config.py
+++ app/config.py
@@ -1,9 +1,10 @@
 import os
-print(os.path.exists(".env"))
+
+# print(os.path.exists("app/.env"))
 from dotenv import load_dotenv
 from pathlib import Path
 
-dotenv_path = Path("app/.env") 
+dotenv_path = Path("app/.env")
 load_dotenv(dotenv_path=dotenv_path)
 if not load_dotenv(dotenv_path=dotenv_path):
     print("Failed to load .env file.")
@@ -17,5 +18,3 @@ DATABASE_USERNAME = os.getenv("DATABASE_USERNAME")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
-
-
